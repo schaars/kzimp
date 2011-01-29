1f089bdaecfe4f25f070330c9587236275844743
Pierre Louis Aublin <pierre-louis.aublin@inria.fr>
2011-01-29 22:43:35 +0100

    Everything's ready for UDP (with and without multicast)

---
+++ microbench_1N/launch_inet_udp.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Args:
+#  $1: nb consumers
+#  $2: message size in B
+#  $3: nb messages warmup phase
+#  $4: nb messages logging phase
+
+
+MEMORY_DIR="memory_conso"
+
+
+# get arguments
+if [ $# -eq 4 ]; then
+   NB_CONSUMERS=$1
+   MSG_SIZE=$2
+   NB_MSG_WARMUP=$3
+   NB_MSG_LOGGING=$4
+else
+   echo "Usage: ./$(basename $0) <nb_consumers> <message_size_in_B> <nb_messages_warmup_phase> <nb_message_logging_phase>"
+   exit 0
+fi
+
+
+./stop_all.sh
+mkdir $MEMORY_DIR
+
+
+# memory for 10 seconds
+./get_memory_usage.sh  $MEMORY_DIR &
+sleep 10
+
+
+# launch XP
+./inet_udp_microbench  -n $NB_CONSUMERS -m $MSG_SIZE -w $NB_MSG_WARMUP -l $NB_MSG_LOGGING
+
+
+# memory for 10 seconds
+sleep 10
+./stop_all.sh
+
+
+# save files
+OUTPUT_DIR="inet_udp_${NB_CONSUMERS}consumers_${MSG_SIZE}B"
+mkdir $OUTPUT_DIR
+mv $MEMORY_DIR $OUTPUT_DIR/
+mv statistics*.log $OUTPUT_DIR/
