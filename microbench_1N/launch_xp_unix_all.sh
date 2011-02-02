c6828ee4aa7cccc9ab9aa3f33487b912bd1566a5
Pierre Louis Aublin <pierre-louis.aublin@inrialpes.fr>
2011-02-02 17:44:39 +0100

    Script modification

--- microbench_1N/launch_xp_unix_all.sh
+++ microbench_1N/launch_xp_unix_all.sh
@@ -6,54 +6,48 @@ for nbdatagrams in 10 1000 100000; do
 for consumers in 1 3 5 7; do
 
 
-warmup=100000
 logging=50000000
 reqsize=64
 
 echo "===== $consumers consumers, $reqsize B ====="
-./launch_unix.sh $consumers $reqsize $warmup $logging $nbdatagrams
+./launch_unix.sh $consumers $reqsize $logging $nbdatagrams
 
 
-warmup=100000
 logging=25000000
 reqsize=1024
 
 echo "===== $consumers consumers, $reqsize B ====="
-./launch_unix.sh $consumers $reqsize $warmup $logging $nbdatagrams
+./launch_unix.sh $consumers $reqsize $logging $nbdatagrams
 
 
-warmup=100000
 logging=10000000
 reqsize=4096
 
 echo "===== $consumers consumers, $reqsize B ====="
-./launch_unix.sh $consumers $reqsize $warmup $logging $nbdatagrams
+./launch_unix.sh $consumers $reqsize $logging $nbdatagrams
 
 
-warmup=100000
 logging=10000000
 reqsize=10240
 
 echo "===== $consumers consumers, $reqsize B ====="
-./launch_unix.sh $consumers $reqsize $warmup $logging $nbdatagrams
+./launch_unix.sh $consumers $reqsize $logging $nbdatagrams
 
 
 
-warmup=100000
 logging=2000000
 reqsize=102400
 
 echo "===== $consumers consumers, $reqsize B ====="
-./launch_unix.sh $consumers $reqsize $warmup $logging $nbdatagrams
+./launch_unix.sh $consumers $reqsize $logging $nbdatagrams
 
 
 
-warmup=10000
 logging=500000
 reqsize=1024000
 
 echo "===== $consumers consumers, $reqsize B ====="
-./launch_unix.sh $consumers $reqsize $warmup $logging $nbdatagrams
+./launch_unix.sh $consumers $reqsize $logging $nbdatagrams
 
 done
 
