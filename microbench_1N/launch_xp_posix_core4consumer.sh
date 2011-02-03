2805c8ce0fa1f75f10deafaf3e21861bcebaafa8
Pierre Louis Aublin <pierre-louis.aublin@inrialpes.fr>
2011-02-03 22:57:50 +0100

    New script for the new XP.

---
+++ microbench_1N/launch_xp_posix_core4consumer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+NUM_CONSUMERS=1
+MSG_SIZE_ARRAY=( 64 1024 10240 102400 1048576 )
+
+XP_DURATION=$((5*60))   # 5 minutes
+
+for core_id in 1 3 5 9; do
+
+cat << EOF > INET_TCP_PROPERTIES
+-DCORE_EXPERIMENT -DCORE_EXPERIMENT_CORE_ID=${core_id}
+EOF
+
+make posix_msg_queue_microbench
+
+for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
+
+	echo "===== $NUM_CONSUMERS consumers, ${XP_DURATION} secondes, msg size is ${MSG_SIZE_ARRAY[$i]}B ====="
+	./launch_posix_msg_queue.sh $NUM_CONSUMERS ${MSG_SIZE_ARRAY[$i]} ${XP_DURATION}
+
+done
+
+mkdir microbench_posix_msg_queue_core${core_id}
+mv microbench_posix_msg_queue_1consumers_* microbench_posix_msg_queue_core${core_id}/
+
+done
