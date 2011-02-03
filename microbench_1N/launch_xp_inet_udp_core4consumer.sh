98267a13f15f6b08fc578e795bb77c2cff1d2542
bft <bft@sci77.inrialpes.fr>
2011-02-03 22:52:04 +0100

    New script for the new experiment.

---
+++ microbench_1N/launch_xp_inet_udp_core4consumer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+NUM_CONSUMERS=1
+MSG_SIZE_ARRAY=( 64 1024 10240 102400 1048576 )
+
+XP_DURATION=$((5*60))   # 5 minutes
+
+for core_id in 1 3 5 9; do
+
+cat << EOF > INET_TCP_PROPERTIES
+-DCORE_EXPERIMENT -DCORE_EXPERIMENT_CORE_ID=${core_id}
+EOF
+
+make inet_udp_microbench
+
+for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
+
+	echo "===== $NUM_CONSUMERS consumers, ${XP_DURATION} secondes, msg size is ${MSG_SIZE_ARRAY[$i]}B ====="
+	./launch_inet_tcp.sh $NUM_CONSUMERS ${MSG_SIZE_ARRAY[$i]} ${XP_DURATION}
+
+done
+
+mkdir microbench_inet_udp_core${core_id}
+mv microbench_inet_udp_1consumers_* microbench_inet_udp_core${core_id}/
+
+done
