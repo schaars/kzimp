d1116fad7efe0903ee3db7ed4a1b3129716b2a5e
Pierre Louis Aublin <pierre-louis.aublin@inrialpes.fr>
2011-02-03 15:34:29 +0100

    Script stuffing.

--- microbench_1N/launch_xp_inet_tcp_all.sh
+++ microbench_1N/launch_xp_inet_tcp_all.sh
@@ -10,7 +10,7 @@ for num_consumers in ${NUM_CONSUMERS_ARRAY[@]}; do
 	for i in $(seq 0 $(( ${#TRANSFER_SIZE_ARRAY[@]}-1 )) ); do
 
 		echo "===== $num_consumers consumers, ${TRANSFER_SIZE_ARRAY[$i]}GB transfered, msg size is ${MSG_SIZE_ARRAY[$i]}B ====="
-		./launch_inet_tcp.sh $num_consumers ${MSG_SIZE_ARRAY[$i]} ${TRANSFER_SIZE_ARRAY[$i]}
+		./launch_inet_tcp.sh $num_consumers ${MSG_SIZE_ARRAY[$i]} $(( ${TRANSFER_SIZE_ARRAY[$i]} * 1000000000 ))
 
 	done
 
