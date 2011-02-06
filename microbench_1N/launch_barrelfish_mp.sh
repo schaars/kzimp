21ef2b692ab386603b3cfcb7940979afa8c7331c
Pierre Louis Aublin <pierre-louis.aublin@inria.fr>
2011-02-06 21:36:58 +0100

    Script for Barrelfish. For now busy waiting, but we do not really care.

--- microbench_1N/launch_barrelfish_mp.sh
+++ microbench_1N/launch_barrelfish_mp.sh
@@ -4,7 +4,7 @@
 #  $1: nb consumers
 #  $2: message size in B
 #  $3: duration of the experiment in seconds
-#  $4: max duration of the experiment in seconds in the channel
+#  $4: max nb of messages in the channel
 
 
 MEMORY_DIR="memory_conso"
@@ -14,9 +14,9 @@ if [ $# -eq 4 ]; then
    NB_CONSUMERS=$1
    MSG_SIZE=$2
    DURATION_XP=$3
-   DURATION_XP_CHANNEL=$4
+   MAX_MSG_CHANNEL=$4
 else
-   echo "Usage: ./$(basename $0) <nb_consumers> <message_size_in_B> <xp_duration_in_sec> <DURATION_XP_channel>"
+   echo "Usage: ./$(basename $0) <nb_consumers> <message_size_in_B> <xp_duration_in_sec> <max_nb_messages_in_channel>"
    exit 0
 fi
 
@@ -25,12 +25,15 @@ rm -rf $MEMORY_DIR && mkdir $MEMORY_DIR
 
 ./stop_all.sh
 
+# used by ftok
+touch /tmp/barrelfish_message_passing_microbench
+
 # modify shared mem parameters
 sudo ./root_set_value.sh 10000000000 /proc/sys/kernel/shmall
 sudo ./root_set_value.sh 10000000000 /proc/sys/kernel/shmmax
 
 # recompile with message size
-echo "-DNB_MESSAGES=$DURATION_XP_CHANNEL -DURPC_MSG_WORDS=$(($MSG_SIZE/8))" > BARRELFISH_MESSAGE_PASSING_PROPERTIES
+echo "-DNB_MESSAGES=$MAX_MSG_CHANNEL -DURPC_MSG_WORDS=$(($MSG_SIZE/8))" > BARRELFISH_MESSAGE_PASSING_PROPERTIES
 make barrelfish_message_passing
 
 # launch XP
@@ -40,7 +43,7 @@ make barrelfish_message_passing
 ./stop_all.sh
 
 # save files
-OUTPUT_DIR="microbench_barrelfish_message_passing_${NB_CONSUMERS}consumers_${DURATION_XP}sec_${MSG_SIZE}B"
+OUTPUT_DIR="microbench_barrelfish_message_passing_${NB_CONSUMERS}consumers_${DURATION_XP}sec_${MSG_SIZE}B_${MAX_MSG_CHANNEL}nb_messages_channel"
 mkdir $OUTPUT_DIR
 mv $MEMORY_DIR $OUTPUT_DIR/
 mv statistics*.log $OUTPUT_DIR/
