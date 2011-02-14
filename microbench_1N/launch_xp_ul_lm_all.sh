21b90646cd149477600e484932ac3efc75421246
Pierre Louis Aublin <pierre-louis.aublin@inria.fr>
2011-02-14 19:12:49 +0100

    Script for User-level Local Multicast XP.

--- microbench_1N/launch_xp_ul_lm_all.sh
+++ microbench_1N/launch_xp_ul_lm_all.sh
@@ -1,83 +1,29 @@
 #!/bin/bash
 
-NUM_CONSUMERS_ARRAY=( 1 )
-MSG_SIZE_ARRAY=( 64 1024 )
-NUM_MSG_BUFFER_ARRAY=( 1000 )
+
+NUM_CONSUMERS_ARRAY=( 1 3 5 7 )
+MSG_SIZE_ARRAY=( 1 64 128 512 1024 10240 102400 1048576 )
 XP_DURATION=$((5*60)) # 5 minutes
 
-for num_msg_channel in ${NUM_MSG_BUFFER_ARRAY[@]}; do
+for num_consumers in ${NUM_CONSUMERS_ARRAY[@]}; do
 
-	for num_consumers in ${NUM_CONSUMERS_ARRAY[@]}; do
+   for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
 
-		for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
+      if [ $num_consumers -eq 1 ]; then
+         if [ ${MSG_SIZE_ARRAY[$i]} -le 1024 ]; then
+            num_msg_channel=1000
+         elif [ ${MSG_SIZE_ARRAY[$i]} -eq 10240 ]; then
+            num_msg_channel=100
+         else
+            num_msg_channel=10
+         fi
+      else
+         num_msg_channel=10
+      fi
 
-			echo "===== $num_consumers consumers, ${XP_DURATION} secondes, msg size is ${MSG_SIZE_ARRAY[$i]}B, $num_msg_channel messages in channel ====="
-			./launch_ul_lm.sh $num_consumers ${MSG_SIZE_ARRAY[$i]} ${XP_DURATION} $num_msg_channel
+      echo "===== $num_consumers consumers, ${XP_DURATION} secondes, msg size is ${MSG_SIZE_ARRAY[$i]}B, $num_msg_channel messages in channel ====="
+      ./launch_ul_lm.sh $num_consumers ${MSG_SIZE_ARRAY[$i]} ${XP_DURATION} $num_msg_channel
 
-		done
-
-	done
-
-done
-
-NUM_CONSUMERS_ARRAY=( 1 )
-MSG_SIZE_ARRAY=( 10240 )
-NUM_MSG_BUFFER_ARRAY=( 100 )
-XP_DURATION=$((5*60)) # 5 minutes
-
-for num_msg_channel in ${NUM_MSG_BUFFER_ARRAY[@]}; do
-
-	for num_consumers in ${NUM_CONSUMERS_ARRAY[@]}; do
-
-		for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
-
-			echo "===== $num_consumers consumers, ${XP_DURATION} secondes, msg size is ${MSG_SIZE_ARRAY[$i]}B, $num_msg_channel messages in channel ====="
-			./launch_ul_lm.sh $num_consumers ${MSG_SIZE_ARRAY[$i]} ${XP_DURATION} $num_msg_channel
-
-		done
-
-	done
-
-done
-
-NUM_CONSUMERS_ARRAY=( 1 )
-MSG_SIZE_ARRAY=( 1048576 )
-NUM_MSG_BUFFER_ARRAY=( 10 )
-XP_DURATION=$((5*60)) # 5 minutes
-
-for num_msg_channel in ${NUM_MSG_BUFFER_ARRAY[@]}; do
-
-	for num_consumers in ${NUM_CONSUMERS_ARRAY[@]}; do
-
-		for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
-
-			echo "===== $num_consumers consumers, ${XP_DURATION} secondes, msg size is ${MSG_SIZE_ARRAY[$i]}B, $num_msg_channel messages in channel ====="
-			./launch_ul_lm.sh $num_consumers ${MSG_SIZE_ARRAY[$i]} ${XP_DURATION} $num_msg_channel
-
-		done
-
-	done
-
-done
-
-
-
-NUM_CONSUMERS_ARRAY=( 3 5 7 )
-MSG_SIZE_ARRAY=( 64 1024 10240 102400 1048576 )
-NUM_MSG_BUFFER_ARRAY=( 10 )
-XP_DURATION=$((5*60)) # 5 minutes
-
-for num_msg_channel in ${NUM_MSG_BUFFER_ARRAY[@]}; do
-
-	for num_consumers in ${NUM_CONSUMERS_ARRAY[@]}; do
-
-		for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
-
-			echo "===== $num_consumers consumers, ${XP_DURATION} secondes, msg size is ${MSG_SIZE_ARRAY[$i]}B, $num_msg_channel messages in channel ====="
-			./launch_ul_lm.sh $num_consumers ${MSG_SIZE_ARRAY[$i]} ${XP_DURATION} $num_msg_channel
-
-		done
-
-	done
+   done
 
 done
