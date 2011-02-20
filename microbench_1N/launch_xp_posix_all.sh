72caca2e7bb0e491484bc0fde6717ef4c1f48b77
Pierre Louis Aublin <pierre-louis.aublin@inria.fr>
2011-02-20 13:42:53 +0100

    Ready for POSIX msg queue!

--- microbench_1N/launch_xp_posix_all.sh
+++ microbench_1N/launch_xp_posix_all.sh
@@ -9,15 +9,15 @@ XP_DURATION=$((5*60))   # 5 minutes
 
 for msg_channel in ${MSG_CHANNEL_ARRAY[@]}; do
 
-for num_consumers in ${NUM_CONSUMERS_ARRAY[@]}; do
+   for num_consumers in ${NUM_CONSUMERS_ARRAY[@]}; do
 
-	for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
+      for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
 
-		echo "===== $num_consumers consumers, ${XP_DURATION} secondes, msg size is ${MSG_SIZE_ARRAY[$i]}B, $msg_channel msg in channel ====="
-		./launch_posix_msg_queue.sh $num_consumers ${MSG_SIZE_ARRAY[$i]} ${XP_DURATION} $msg_channel
+         echo "===== $num_consumers consumers, ${XP_DURATION} secondes, msg size is ${MSG_SIZE_ARRAY[$i]}B, $msg_channel msg in channel ====="
+         ./launch_posix_msg_queue.sh $num_consumers ${MSG_SIZE_ARRAY[$i]} ${XP_DURATION} $msg_channel
 
-	done
+      done
 
-done
+   done
 
 done
--- microbench_1N/src/posix_msg_queue.c
+++ microbench_1N/src/posix_msg_queue.c
@@ -44,189 +44,192 @@ static mqd_t consumer_queue; // pointer to this consumer's queue for reading
 // First initialization function called
 void IPC_initialize(int _nb_receivers, int _request_size)
 {
-  nb_receivers = _nb_receivers;
+   nb_receivers = _nb_receivers;
 
-  request_size = _request_size;
-  if (request_size < MIN_MSG_SIZE)
-  {
-    request_size = MIN_MSG_SIZE;
-  }
+   request_size = _request_size;
+   if (request_size < MIN_MSG_SIZE)
+   {
+      request_size = MIN_MSG_SIZE;
+   }
 
-  nb_cycles_send = 0;
-  nb_cycles_recv = 0;
-  nb_cycles_first_recv = 0;
+   nb_cycles_send = 0;
+   nb_cycles_recv = 0;
+   nb_cycles_first_recv = 0;
 
-  consumers = (mqd_t*) malloc(sizeof(mqd_t) * nb_receivers);
-  if (!consumers)
-  {
-    perror("Allocation error");
-    exit(errno);
-  }
-
-
-/****** get message queues properties ******/
-struct rlimit rlim;
-int res;
-
-res = getrlimit(RLIMIT_MSGQUEUE, &rlim);
-if (res == -1) {
-    perror("getrlimit error");
-} else {
-printf("===== BEFORE MODIFICATION\nsoft=%i, hard=%i\n", (int)rlim.rlim_cur, (int)rlim.rlim_max);
-}
+   consumers = (mqd_t*) malloc(sizeof(mqd_t) * nb_receivers);
+   if (!consumers)
+   {
+      perror("Allocation error");
+      exit(errno);
+   }
 
 
-rlim.rlim_cur = rlim.rlim_max = 1073741824;
-res = setrlimit(RLIMIT_MSGQUEUE, &rlim);
-if (res == -1) {
-    perror("setrlimit error");
-}
-
-
-res = getrlimit(RLIMIT_MSGQUEUE, &rlim);
-if (res == -1) {
-    perror("getrlimit error");
-} else {
-printf("===== AFTER MODIFICATION\nsoft=%i, hard=%i\n", (int)rlim.rlim_cur, (int)rlim.rlim_max);
-}
-
-
-
-  int i;
-  char filename[256];
-
-  msg_max_size_in_queue = 0;
-  for (i = 0; i < nb_receivers; i++)
-  {
-    sprintf(filename, "/posix_message_queue_microbench%i", i + 1);
-
-    if ((consumers[i] = mq_open(filename, O_RDWR | O_CREAT, 0600, NULL)) == -1)
-    {
-      perror("mq_open");
-      exit(1);
-    }
-
-    struct mq_attr attr;
-    int ret = mq_getattr(consumers[i], &attr);
-    if (!ret)
-    {
-      msg_max_size_in_queue = MAX(msg_max_size_in_queue, attr.mq_msgsize);
+   /****** get and set message queues properties ******/
+   struct rlimit rlim;
+   int res;
 
 #ifdef DEBUG
-      printf(
-          "New queue. Attributes are:\n\tflags=%li, maxmsg=%li, msgsize=%li, curmsgs=%li\n",
-          attr.mq_flags, attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
+   res = getrlimit(RLIMIT_MSGQUEUE, &rlim);
+   if (res == -1) {
+      perror("getrlimit error");
+   } else {
+      printf("===== BEFORE MODIFICATION\nsoft=%i, hard=%i\n", (int)rlim.rlim_cur, (int)rlim.rlim_max);
+   }
 #endif
-    }
-    else
-    {
-      perror("mq_getattr");
-    }
-  }
+
+
+   rlim.rlim_cur = rlim.rlim_max = RLIM_INFINITY;
+   res = setrlimit(RLIMIT_MSGQUEUE, &rlim);
+   if (res == -1) {
+      perror("setrlimit error");
+      exit(-1);
+   }
+
+#ifdef DEBUG
+   res = getrlimit(RLIMIT_MSGQUEUE, &rlim);
+   if (res == -1) {
+      perror("getrlimit error");
+   } else {
+      printf("===== AFTER MODIFICATION\nsoft=%i, hard=%i\n", (int)rlim.rlim_cur, (int)rlim.rlim_max);
+   }
+#endif
+
+
+   int i;
+   char filename[256];
+
+   msg_max_size_in_queue = 0;
+   for (i = 0; i < nb_receivers; i++)
+   {
+      sprintf(filename, "/posix_message_queue_microbench%i", i + 1);
+
+      if ((consumers[i] = mq_open(filename, O_RDWR | O_CREAT, 0600, NULL)) == -1)
+      {
+         perror("mq_open");
+         exit(1);
+      }
+
+      struct mq_attr attr;
+      int ret = mq_getattr(consumers[i], &attr);
+      if (!ret)
+      {
+         msg_max_size_in_queue = MAX(msg_max_size_in_queue, attr.mq_msgsize);
+
+#ifdef DEBUG
+         printf(
+               "New queue. Attributes are:\n\tflags=%li, maxmsg=%li, msgsize=%li, curmsgs=%li\n",
+               attr.mq_flags, attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
+#endif
+      }
+      else
+      {
+         perror("mq_getattr");
+      }
+   }
 }
 
 // Initialize resources for the producer
 void IPC_initialize_producer(int _core_id)
 {
-  core_id = _core_id;
+   core_id = _core_id;
 }
 
 // Initialize resources for the consumers
 void IPC_initialize_consumer(int _core_id)
 {
-  core_id = _core_id;
+   core_id = _core_id;
 
-  consumer_queue = consumers[core_id - 1];
+   consumer_queue = consumers[core_id - 1];
 }
 
 // Clean ressources created for both the producer and the consumer.
 // Called by the parent process, after the death of the children.
 void IPC_clean(void)
 {
-  int i;
+   int i;
 
-  sleep(1);
+   sleep(1);
 
-  char filename[256];
-  for (i = 0; i < nb_receivers; i++)
-  {
-    sprintf(filename, "/posix_message_queue_microbench%i", i + 1);
-    mq_unlink(filename);
-  }
+   char filename[256];
+   for (i = 0; i < nb_receivers; i++)
+   {
+      sprintf(filename, "/posix_message_queue_microbench%i", i + 1);
+      mq_unlink(filename);
+   }
 }
 
 // Clean ressources created for the producer.
 void IPC_clean_producer(void)
 {
-  int i;
+   int i;
 
-  for (i = 0; i < nb_receivers; i++)
-  {
-    mq_close(consumers[i]);
-  }
+   for (i = 0; i < nb_receivers; i++)
+   {
+      mq_close(consumers[i]);
+   }
 }
 
 // Clean ressources created for the consumer.
 void IPC_clean_consumer(void)
 {
-  IPC_clean_producer();
+   IPC_clean_producer();
 }
 
 // Return the number of cycles spent in the send() operation
 uint64_t get_cycles_send()
 {
-  return nb_cycles_send;
+   return nb_cycles_send;
 }
 
 // Return the number of cycles spent in the recv() operation
 uint64_t get_cycles_recv()
 {
-  return nb_cycles_recv - nb_cycles_first_recv;
+   return nb_cycles_recv - nb_cycles_first_recv;
 }
 
 // Send a message to all the cores
 // The message id will be msg_id
 void IPC_sendToAll(int msg_size, char msg_id)
 {
-  uint64_t cycle_start, cycle_stop;
-  int i;
-  char *msg;
+   uint64_t cycle_start, cycle_stop;
+   int i;
+   char *msg;
 
-  if (msg_size < MIN_MSG_SIZE)
-  {
-    msg_size = MIN_MSG_SIZE;
-  }
+   if (msg_size < MIN_MSG_SIZE)
+   {
+      msg_size = MIN_MSG_SIZE;
+   }
 
-  msg = (char*) malloc(GET_MALLOC_SIZE(sizeof(char) * msg_size));
-  if (!msg)
-  {
-    perror("IPC_sendToAll allocation error! ");
-    exit(errno);
-  }
+   msg = (char*) malloc(GET_MALLOC_SIZE(sizeof(char) * msg_size));
+   if (!msg)
+   {
+      perror("IPC_sendToAll allocation error! ");
+      exit(errno);
+   }
 
-  // malloc is lazy: the pages may not be really allocated yet.
-  // We force the allocation and the fetch of the pages with bzero
-  bzero(msg, msg_size);
+   // malloc is lazy: the pages may not be really allocated yet.
+   // We force the allocation and the fetch of the pages with bzero
+   bzero(msg, msg_size);
 
-  msg[0] = msg_id;
+   msg[0] = msg_id;
 
 #ifdef DEBUG
-  printf(
-      "[producer %i] going to send message %i of size %i to %i recipients\n",
-      core_id, msg[0], msg_size, nb_receivers);
+   printf(
+         "[producer %i] going to send message %i of size %i to %i recipients\n",
+         core_id, msg[0], msg_size, nb_receivers);
 #endif
 
-  for (i = 0; i < nb_receivers; i++)
-  {
-    // writing the content
-    rdtsc(cycle_start);
-    mq_send(consumers[i], msg, msg_size, 0);
-    rdtsc(cycle_stop);
+   for (i = 0; i < nb_receivers; i++)
+   {
+      // writing the content
+      rdtsc(cycle_start);
+      mq_send(consumers[i], msg, msg_size, 0);
+      rdtsc(cycle_stop);
 
-    nb_cycles_send += cycle_stop - cycle_start;
-  }
+      nb_cycles_send += cycle_stop - cycle_start;
+   }
 
-  free(msg);
+   free(msg);
 }
 
 // Get a message for this core
@@ -234,54 +237,54 @@ void IPC_sendToAll(int msg_size, char msg_id)
 // Place in *msg_id the id of this message
 int IPC_receive(int msg_size, char *msg_id)
 {
-  char *msg;
+   char *msg;
 
-  if (msg_size < MIN_MSG_SIZE)
-  {
-    msg_size = MIN_MSG_SIZE;
-  }
+   if (msg_size < MIN_MSG_SIZE)
+   {
+      msg_size = MIN_MSG_SIZE;
+   }
 
-  msg = (char*) malloc(sizeof(char) * msg_max_size_in_queue);
-  if (!msg)
-  {
-    perror("IPC_receive allocation error! ");
-    exit(errno);
-  }
-
-#ifdef DEBUG
-  printf("Waiting for a new message\n");
-#endif
-
-  uint64_t cycle_start, cycle_stop;
-
-  rdtsc(cycle_start);
-  int recv_size = mq_receive(consumer_queue, msg, msg_max_size_in_queue, NULL);
-  rdtsc(cycle_stop);
-
-  nb_cycles_recv += cycle_stop - cycle_start;
-
-  if (nb_cycles_first_recv == 0)
-  {
-    nb_cycles_first_recv = nb_cycles_recv;
-  }
-
-  // get the id of the message
-  *msg_id = msg[0];
+   msg = (char*) malloc(sizeof(char) * msg_max_size_in_queue);
+   if (!msg)
+   {
+      perror("IPC_receive allocation error! ");
+      exit(errno);
+   }
 
 #ifdef DEBUG
-  printf(
-      "[consumer %i] received message %i of size %i, should be %i\n",
-      core_id, *msg_id, recv_size, msg_size);
+   printf("Waiting for a new message\n");
 #endif
 
-  free(msg);
+   uint64_t cycle_start, cycle_stop;
 
-  if (recv_size == msg_size)
-  {
-    return msg_size;
-  }
-  else
-  {
-    return 0;
-  }
+   rdtsc(cycle_start);
+   int recv_size = mq_receive(consumer_queue, msg, msg_max_size_in_queue, NULL);
+   rdtsc(cycle_stop);
+
+   nb_cycles_recv += cycle_stop - cycle_start;
+
+   if (nb_cycles_first_recv == 0)
+   {
+      nb_cycles_first_recv = nb_cycles_recv;
+   }
+
+   // get the id of the message
+   *msg_id = msg[0];
+
+#ifdef DEBUG
+   printf(
+         "[consumer %i] received message %i of size %i, should be %i\n",
+         core_id, *msg_id, recv_size, msg_size);
+#endif
+
+   free(msg);
+
+   if (recv_size == msg_size)
+   {
+      return msg_size;
+   }
+   else
+   {
+      return 0;
+   }
 }
