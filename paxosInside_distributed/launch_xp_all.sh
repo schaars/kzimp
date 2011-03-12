0d9b080af9d551983ad76997764f6297b6baabf7
Pierre Louis Aublin <pierre-louis.aublin@inrialpes.fr>
2011-03-12 15:30:11 +0100

    Fixing scripts

--- paxosInside_distributed/launch_xp_all.sh
+++ paxosInside_distributed/launch_xp_all.sh
@@ -2,11 +2,20 @@
 
 NB_PAXOS_NODES=5
 NB_ITER=10000000
-LEADER_ACCEPTOR=same_proc
-#MSG_SIZE_ARRAY=( 64 128 512 1024 4096 10240 102400 1048576 )
-MSG_SIZE_ARRAY=( 64 128 512 1024 4096 10240 )
+LEADER_ACCEPTOR_ARRAY=( same_proc different_proc )
+MSG_SIZE_ARRAY=( 64 128 512 1024 4096 10240 102400 1048576 )
 
-for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
-  ./launch_ulm.sh $NB_PAXOS_NODES $NB_ITER $LEADER_ACCEPTOR ${MSG_SIZE_ARRAY[$i]} 50
-  ./launch_barrelfish_mp.sh $NB_PAXOS_NODES $NB_ITER $LEADER_ACCEPTOR ${MSG_SIZE_ARRAY[$i]} 1000
+for leader_acceptor in ${LEADER_ACCEPTOR_ARRAY[@]}; do
+for msg_size in ${MSG_SIZE_ARRAY[@]}; do
+
+if [ $msg_size -eq 102400 ] || [ $msg_size -eq 1048576 ]; then
+  NB_ITER=10000
+else
+  NB_ITER=10000000
+fi  
+    
+  ./launch_ulm.sh $NB_PAXOS_NODES $NB_ITER $leader_acceptor ${MSG_SIZE_ARRAY[$i]} 50
+  ./launch_barrelfish_mp.sh $NB_PAXOS_NODES $NB_ITER $leader_acceptor ${MSG_SIZE_ARRAY[$i]} 1000
+  
+done
 done
