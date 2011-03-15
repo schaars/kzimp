58fd35170d0d95b7afc866f3f1934dde8e7bf914
bft on Bertha <bertha@inria.fr>
2011-03-15 22:05:05 +0100

    Iterations at 10k

--- checkpointing/launch_xp_all.sh
+++ checkpointing/launch_xp_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NB_NODES_ARRAY=( 2 3 5 7 )
-NB_ITER=1000000
+NB_NODES_ARRAY=( 2 3 5 7 9 11 13 15 )
+NB_ITER=10000000
 MSG_SIZE_ARRAY=( 64 128 512 1024 4096 10240 102400 1048576 )
 
 for nb_nodes in ${NB_NODES_ARRAY[@]}; do
@@ -9,8 +9,10 @@ for msg_size in ${MSG_SIZE_ARRAY[@]}; do
 
   if [ $msg_size -eq 102400 ] || [ $msg_size -eq 1048576 ]; then
     NB_ITER=10000
-  else
+  elif [ $msg_size -eq 10240 ]; then
     NB_ITER=1000000
+  else
+    NB_ITER=10000000
   fi 
 
   ./launch_ulm.sh $nb_nodes $NB_ITER $msg_size 50
