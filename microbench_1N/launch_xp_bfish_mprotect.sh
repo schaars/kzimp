792627e6d1ab15278a9b6ff13cb328c9c035fe7d
Pierre Louis Aublin <pierre-louis.aublin@inria.fr>
2011-07-17 11:59:34 +0200

    scripts for bfish mprotect (profiling is missing)

---
+++ microbench_1N/launch_xp_bfish_mprotect.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+NUM_CONSUMERS_ARRAY=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 )
+MSG_SIZE_ARRAY=( 1 64 128 512 1024 4096 10240 102400 1048576 )
+NUM_MSG_CHANNEL_ARRAY=( 50 )
+XP_DURATION=$((2*60)) # 2 minutes
+
+for num_msg_channel in ${NUM_MSG_CHANNEL_ARRAY[@]}; do
+
+   for num_consumers in ${NUM_CONSUMERS_ARRAY[@]}; do
+
+      for i in $(seq 0 $(( ${#MSG_SIZE_ARRAY[@]}-1 )) ); do
+
+         echo "===== $(date) $num_consumers consumers, ${XP_DURATION} secondes, msg size is ${MSG_SIZE_ARRAY[$i]}B, $num_msg_channel messages in channel ====="
+         ./launch_bfish_mprotect.sh $num_consumers ${MSG_SIZE_ARRAY[$i]} ${XP_DURATION} $num_msg_channel
+
+      done
+
+   done
+
+done
---
+++ microbench_1N/scripts_throughput/launch_bfish_mprotect.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Args:
+#  $1: nb consumers
+#  $2: message size in B
+#  $3: duration of the experiment in seconds
+#  $4: max nb of messages in the circular buffer
+
+
+KBFISH_MEM_DIR="../kbfishmem"
+BFISH_MPROTECT_DIR="../kzimp/kzimp"
+
+
+# get arguments
+if [ $# -eq 4 ]; then
+   NB_CONSUMERS=$1
+   MSG_SIZE=$2
+   DURATION_XP=$3
+   MAX_NB_MSG=$4
+else
+   echo "Usage: ./$(basename $0) <nb_consumers> <message_size_in_B> <xp_duration_in_sec> <max_nb_messages_in_circular_buffer>"
+   exit 0
+fi
+
+OUTPUT_DIR="microbench_bfish_mprotect_${NB_CONSUMERS}consumers_${DURATION_XP}sec_${MSG_SIZE}B_${MAX_NB_MSG}messages_in_buffer"
+
+if [ -d $OUTPUT_DIR ]; then
+   echo KZIMP ${NB_CONSUMERS} consumers, ${DURATION_XP} sec, ${MSG_SIZE}B ${MAX_NB_MSG} msg in channel already done
+   exit 0
+fi
+
+#rm -rf $MEMORY_DIR && mkdir $MEMORY_DIR
+
+./stop_all.sh
+
+#compile and load module
+cd $KBFISH_MEM_DIR
+make
+./kbfishmem.sh load nb_max_communication_channels=1 default_channel_size=${MAX_NB_MSG} default_max_msg_size=${MSG_SIZE} 
+if [ $? -eq 1 ]; then
+   echo "An error has occured when loading kbfishmem. Aborting the experiment $OUTPUT_DIR"
+   exit 0
+fi
+cd -
+
+# launch XP
+#./get_memory_usage.sh  $MEMORY_DIR &
+make bfish_mprotect_microbench
+timelimit -p -s 9 -t $((${DURATION_XP}+30)) ./bin/bfish_mprotect_microbench -r $NB_CONSUMERS -s $MSG_SIZE -t $DURATION_XP
+
+./stop_all.sh
+sleep 1
+cd $KBFISH_MEM_DIR; ./kbfishmem.sh unload; cd -
+
+# save files
+mkdir $OUTPUT_DIR
+#mv $MEMORY_DIR $OUTPUT_DIR/
+mv statistics*.log $OUTPUT_DIR/
