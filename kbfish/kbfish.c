e5f3f863afb55e1b8ab3a9c7d4a525e7d2ee8ced
Pierre Louis Aublin <pierre-louis.aublin@inria.fr>
2011-07-17 23:27:37 +0200

    work in progress on kbfish, which is a kernel version of barrelfish message passing

---
+++ kbfish/kbfish.c
@@ -0,0 +1,315 @@
+/* kbfish - character device module */
+
+#include <linux/kernel.h>      /* Needed for KERN_INFO */
+#include <linux/init.h>        /* Needed for the macros */
+#include <linux/proc_fs.h>     /* proc fs */
+#include <linux/sched.h>       /* current macro */
+#include <linux/fcntl.h>       /* some flags */
+#include <asm/uaccess.h>       /* copy_from_user function */
+#include <linux/vmalloc.h>     /* vmalloc */
+
+#include "kbfish.h"
+
+// debug flag
+#undef DEBUG
+
+// character device files need a major and minor number.
+// They are set automatically when loading the module
+static int kbfish_major, kbfish_minor;
+
+// holds device information
+static dev_t kbfish_dev_t;
+
+// pointer to the /proc file
+static struct proc_dir_entry *proc_file;
+
+// array of the kbfish_dev
+static struct kbfish_channel *channels;
+
+/*
+ * kbfish open operation.
+ * The file must be opened in RW mode.
+ * The receiver needs to set the O_CREAT flag,
+ * otherwise it will be considered as a sender.
+ * Returns:
+ *  . -ENOMEM memory allocation failed
+ *  . -EACCESS if the requested access is not allowed (must be RW)
+ *  . -EEXIST if there is already a registered process
+ *  . 0 otherwise
+ */
+static int kbfish_open(struct inode *inode, struct file *filp)
+{
+  struct kbfish_channel *chan; /* channel information */
+  struct kbfish_ctrl *ctrl;
+  int retval;
+
+  // the file must be opened in RW, otherwise we cannot mmap the areas
+  if (!(filp->f_mode & FMODE_READ) && !(filp->f_mode & FMODE_WRITE))
+  {
+    printk(KERN_ERR "kbfish: process %i in open has not the right credentials\n", current->pid);
+    retval = -EACCES;
+    goto out;
+  }
+
+  chan = container_of(inode->i_cdev, struct kbfish_channel, cdev);
+
+  ctrl = kmalloc(sizeof(*ctrl), GFP_KERNEL);
+  if (unlikely(!ctrl))
+  {
+    printk(KERN_ERR "kbfish: kbfish_ctrl allocation error\n");
+    return -ENOMEM;
+  }
+
+  spin_lock(&chan->bcl);
+
+  // the receiver needs the O_CREAT flag
+  if (filp->f_flags & O_CREAT)
+  {
+    if (chan->receiver != -1)
+    {
+      printk(KERN_ERR "kbfish: process %i in open but there is already a receiver: %i\n", current->pid, chan->receiver);
+      retval = -EEXIST;
+      goto unlock;
+    }
+    chan->receiver = current->pid;
+    ctrl->is_sender = 0;
+
+    // set the 2 areas to 0
+    memset(chan->sender_to_receiver, 0, chan->size_in_bytes);
+    memset(chan->receiver_to_sender, 0, chan->size_in_bytes);
+  }
+  else
+  {
+    if (chan->sender != -1)
+    {
+      printk(KERN_ERR "kbfish: process %i in open but there is already a sender: %i\n", current->pid, chan->sender);
+      retval = -EEXIST;
+      goto unlock;
+    }
+    chan->sender = current->pid;
+    ctrl->is_sender = 1;
+  }
+
+  ctrl->pid = current->pid;
+  ctrl->chan = chan;
+  filp->private_data = ctrl;
+  retval = 0;
+
+  unlock: spin_unlock(&chan->bcl);
+  out: return retval;
+}
+
+/*
+ * kbfish release operation.
+ * Returns:
+ *  . 0: it always succeeds
+ */
+static int kbfish_release(struct inode *inode, struct file *filp)
+{
+  struct kbfish_channel *chan; /* channel information */
+  struct kbfish_ctrl *ctrl;
+
+  ctrl = filp->private_data;
+  chan = ctrl->chan;
+
+  spin_lock(&chan->bcl);
+
+  if (ctrl->is_sender)
+  {
+    chan->sender = -1;
+  }
+  else
+  {
+    chan->receiver = -1;
+  }
+
+  spin_unlock(&chan->bcl);
+
+  kfree(ctrl);
+
+  return 0;
+}
+
+static int kbfish_init_channel(struct kbfish_channel *channel, int chan_id,
+    int max_msg_size, int channel_size, int init_lock)
+{
+  channel->chan_id = chan_id;
+  channel->sender = -1;
+  channel->receiver = -1;
+  channel->channel_size = channel_size;
+  channel->max_msg_size = max_msg_size;
+  channel->size_in_bytes = ROUND_UP_SIZE(channel_size * max_msg_size);
+  channel->sender_to_receiver = vmalloc(channel->size_in_bytes);
+  channel->receiver_to_sender = vmalloc(channel->size_in_bytes);
+  if (!channel->sender_to_receiver || !channel->receiver_to_sender)
+  {
+    printk(KERN_ERR "kbfish: vmalloc error of %lu bytes: %p %p\n", channel->size_in_bytes, channel->sender_to_receiver, channel->receiver_to_sender);
+    return -1;
+  }
+
+  if (init_lock)
+  {
+    channel->bcl = SPIN_LOCK_UNLOCKED;
+  }
+
+  return 0;
+}
+
+// called when reading file /proc/<procfs_name>
+static int kbfish_read_proc_file(char *page, char **start, off_t off,
+    int count, int *eof, void *data)
+{
+  int len, i;
+
+  len = sprintf(page, "kbfish %s @ %s\n\n", __DATE__, __TIME__);
+  len += sprintf(page + len, "page size = %lu\n", PAGE_SIZE);
+  len += sprintf(page + len, "nb_max_communication_channels = %i\n",
+      nb_max_communication_channels);
+  len += sprintf(page + len, "default_channel_size = %i\n",
+      default_channel_size);
+  len += sprintf(page + len, "default_max_msg_size = %i\n\n",
+      default_max_msg_size);
+
+  len
+  += sprintf(page + len,
+      "chan_id\tchan_size\tmax_msg_size\tsize_in_bytes\tpid_sender\tpid_receiver\n");
+  for (i = 0; i < nb_max_communication_channels; i++)
+  {
+    len += sprintf(page + len, "%i\t%i\t%i\t%lu\t%i\t%i\n",
+        channels[i].chan_id, channels[i].channel_size,
+        channels[i].max_msg_size, channels[i].size_in_bytes,
+        channels[i].sender, channels[i].receiver);
+  }
+
+  return len;
+}
+
+// called when writing to file /proc/<procfs_name>
+static int kbfish_write_proc_file(struct file *file, const char *buffer,
+    unsigned long count, void *data)
+{
+  int len;
+  char* kbuff;
+
+  len = count;
+
+  kbuff = (char*) kmalloc(sizeof(char) * len, GFP_KERNEL);
+
+  if (copy_from_user(kbuff, buffer, len))
+  {
+    kfree(kbuff);
+    return -EFAULT;
+  }
+
+  kbuff[len - 1] = '\0';
+
+  //TODO: sscanf(kbuff, "", ...);
+  //for a channel, change the parameters
+
+  kfree(kbuff);
+
+  return len;
+}
+
+static int kbfish_init_cdev(struct kbfish_channel *channel, int i)
+{
+  int err, devno;
+
+  err = kbfish_init_channel(channel, i, default_max_msg_size,
+      default_channel_size, 1);
+  if (unlikely(err))
+  {
+    printk(KERN_ERR "kbfish: Error %i at initialization of channel %i", err, i);
+    return -1;
+  }
+
+  devno = MKDEV(kbfish_major, kbfish_minor + i);
+
+  cdev_init(&channel->cdev, &kbfish_fops);
+  channel->cdev.owner = THIS_MODULE;
+
+  err = cdev_add(&channel->cdev, devno, 1);
+  /* Fail gracefully if need be */
+  if (unlikely(err))
+  {
+    printk(KERN_ERR "kbfish: Error %d adding kbfish%d", err, i);
+    return -1;
+  }
+
+  return 0;
+}
+
+static void kbfish_del_cdev(struct kbfish_channel *channel)
+{
+  vfree(channel->sender_to_receiver);
+  vfree(channel->receiver_to_sender);
+
+  cdev_del(&channel->cdev);
+}
+
+static int __init kbfish_start(void)
+{
+  int i;
+  int result;
+
+  // ADDING THE DEVICE FILES
+  result = alloc_chrdev_region(&kbfish_dev_t, kbfish_minor, nb_max_communication_channels, DEVICE_NAME);
+  kbfish_major = MAJOR(kbfish_dev_t);
+
+  if (unlikely(result < 0))
+  {
+    printk(KERN_ERR "kbfish: can't get major %d\n", kbfish_major);
+    return result;
+  }
+
+  channels = kmalloc(nb_max_communication_channels * sizeof(struct kbfish_channel), GFP_KERNEL);
+  if (unlikely(!channels))
+  {
+    printk(KERN_ERR "kbfish: channels allocation error\n");
+    return -ENOMEM;
+  }
+
+  for (i=0; i<nb_max_communication_channels; i++)
+  {
+    result = kbfish_init_cdev(&channels[i], i);
+    if (unlikely(result))
+    {
+      printk (KERN_ERR "kbfish: creation of channel device %i failed\n", i);
+      return -1;
+    }
+  }
+
+  // CREATE /PROC FILE
+  proc_file = create_proc_entry(procfs_name, 0444, NULL);
+  if (unlikely(!proc_file))
+  {
+    remove_proc_entry(procfs_name, NULL);
+    printk (KERN_ERR "kbfish: creation of /proc/%s file failed\n", procfs_name);
+    return -1;
+  }
+  proc_file->read_proc = kbfish_read_proc_file;
+  proc_file->write_proc = kbfish_write_proc_file;
+
+  return 0;
+}
+
+// Note: we assume there are no processes still using the channels.
+static void __exit kbfish_end(void)
+{
+  int i;
+
+  // delete channels
+  for (i=0; i<nb_max_communication_channels; i++)
+  {
+    kbfish_del_cdev(&channels[i]);
+  }
+  kfree(channels);
+
+  // remove the /proc file
+  remove_proc_entry(procfs_name, NULL);
+
+  unregister_chrdev_region(kbfish_dev_t, nb_max_communication_channels);
+}
+
+module_init( kbfish_start);
+module_exit( kbfish_end);
---
+++ kbfish/kbfish.h
@@ -0,0 +1,96 @@
+/* Credits:
+ *  -The basic lines come from Linux Device Drivers 3rd edition
+ *
+ *  Developed for an x86-64: we allocate all the memory in ZONE_NORMAL.
+ *  On a x86 machine, ZONE_NORMAL is up to 896MB. On a x86-64 machine, ZONE_NORMAL
+ *  represents the whole memory.
+ */
+
+#ifndef _KZIMP_MODULE_
+#define _KZIMP_MODULE_
+
+#include <linux/module.h>       /* Needed by all modules */
+#include <linux/moduleparam.h>  /* This module takes arguments */
+#include <linux/fs.h>           /* (un)register the block device - file operations */
+#include <linux/cdev.h>         /* char device */
+
+#define DRIVER_AUTHOR "Pierre Louis Aublin <pierre-louis.aublin@inria.fr>"
+#define DRIVER_DESC   "Kernel module of the Barrelfish Message Passing (UMP) communication mechanism"
+
+// even if all the machines do not necessarily have lines of 64B, we don't really care
+#define CACHE_LINE_SIZE 64
+
+// Used to define the size of the pad member
+// The last modulo is to prevent the padding to add CACHE_LINE_SIZE bytes to the structure
+#define PADDING_SIZE(S) ((CACHE_LINE_SIZE - S % CACHE_LINE_SIZE) % CACHE_LINE_SIZE)
+
+// Round up S to a multiple of the page size
+#define ROUND_UP_SIZE(S) (S + (PAGE_SIZE - S % PAGE_SIZE) % PAGE_SIZE)
+
+// This module takes the following arguments:
+static int nb_max_communication_channels = 4;
+module_param(nb_max_communication_channels, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(nb_max_communication_channels, " The max number of communication channels.");
+
+static int default_channel_size = 10;
+module_param(default_channel_size, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(default_channel_size, " The default size of the new channels.");
+
+static int default_max_msg_size = 1024;
+module_param(default_max_msg_size, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(default_max_msg_size, " The default max size of the new channels messages.");
+
+
+// file /dev/<DEVICE_NAME>
+#define DEVICE_NAME "kbfish"
+
+/* file /proc/<procfs_name> */
+#define procfs_name "kbfish"
+
+// FILE OPERATIONS
+static int kbfish_open(struct inode *, struct file *);
+static int kbfish_release(struct inode *, struct file *);
+//TODO: read
+//TODO: write
+//TODO: select
+
+// an open file is associated with a set of functions
+static struct file_operations kbfish_fops =
+{
+    .owner = THIS_MODULE,
+    .open = kbfish_open,
+    .release = kbfish_release,
+//TODO: read
+//TODO: write
+//TODO: select
+};
+
+
+// what is a channel (for this module)
+struct kbfish_channel {
+  int chan_id;                 /* id of this channel */
+  pid_t sender;                /* pid of the sender */
+  pid_t receiver;              /* pid of the receiver */
+  int channel_size;            /* max number of messages */
+  unsigned long size_in_bytes; /* channel size in bytes. Is a multiple of the page size */
+  int max_msg_size;            /* max message size */;
+  spinlock_t bcl;              /* the Big Channel Lock :) */
+  char* sender_to_receiver;    /* shared area used by the sender to send messages */
+  char* receiver_to_sender;    /* shared area used by the receiver to send messages */
+
+  struct cdev cdev;            /* char device structure */
+};
+
+// Each process has a control structure.
+struct kbfish_ctrl {
+  pid_t pid;                   /* pid of this structure's owner */
+  struct kbfish_channel *chan; /* pointer to the channel */
+  int is_sender;               /* is this process a sender? */
+};
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+
+#endif
