ec197fa291a02a2862922ea08436fec719f4f056
Pierre Louis Aublin <pierre-louis.aublin@inria.fr>
2011-07-18 14:29:33 +0200

    removing launch_xp_profiling.sh script, the code is merged with the one of launch_xp_all.sh

--- paxosInside_distributed/launch_xp_all.sh
+++ paxosInside_distributed/launch_xp_all.sh
@@ -5,13 +5,25 @@ NB_ITER=100000
 LEADER_ACCEPTOR_ARRAY=( same_proc different_proc )
 MSG_SIZE_ARRAY=( 64 128 512 1024 4096 10240 102400 1048576 )
 
+# set it to profile if you want to enable the profiling
+PROFILING=
+
 for leader_acceptor in ${LEADER_ACCEPTOR_ARRAY[@]}; do
 for msg_size in ${MSG_SIZE_ARRAY[@]}; do
-    
-  ./launch_ulm.sh $NB_PAXOS_NODES $NB_ITER $leader_acceptor $msg_size 50
-  ./launch_barrelfish_mp.sh $NB_PAXOS_NODES $NB_ITER $leader_acceptor $msg_size 1000
-  ./launch_kzimp.sh $NB_PAXOS_NODES $NB_ITER $leader_acceptor $msg_size 10000
-  ./launch_bfish_mprotect.sh $NB_PAXOS_NODES $NB_ITER $leader_acceptor $msg_size 1000
-  
+
+# may be needed for the profiling, if it takes too much time
+#if [ $msg_size -eq 102400 ] || [ $msg_size -eq 1048576 ]; then
+#  NB_ITER=10000
+#elif [ $msg_size -eq 10240 ]; then
+#  NB_ITER=1000000
+#else
+#  NB_ITER=10000000
+#fi  
+     
+  ./launch_ulm.sh $NB_PAXOS_NODES $NB_ITER $leader_acceptor $msg_size 50 $PROFILING
+  ./launch_barrelfish_mp.sh $NB_PAXOS_NODES $NB_ITER $leader_acceptor $msg_size 1000 $PROFILING
+  ./launch_kzimp.sh $NB_PAXOS_NODES $NB_ITER $leader_acceptor $msg_size 10000 $PROFILING
+  ./launch_bfish_mprotect.sh $NB_PAXOS_NODES $NB_ITER $leader_acceptor $msg_size 1000 $PROFILING
+
 done
 done
