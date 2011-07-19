2099fb3937b8253675800ab8f71b612c19f7c501
Pierre Louis Aublin <pierre-louis.aublin@inria.fr>
2011-07-19 14:27:09 +0200

    tcp paxosInside script

---
+++ paxosInside_distributed/launch_inet_tcp.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#
+# Launch a PaxosInside XP with TCP
+# Args:
+#   $1: nb paxos nodes
+#   $2: nb iter per client
+#   $3: same_proc or different_proc
+#   $4: message max size
+#   $5: if given, then activate profiling
+
+
+CONFIG_FILE=config
+PROFDIR=../profiler
+
+
+if [ $# -eq 5 ]; then
+   NB_PAXOS_NODES=$1
+   NB_ITER=$2
+   LEADER_ACCEPTOR=$3
+   MESSAGE_MAX_SIZE=$4
+   PROFILER=$5
+   
+elif [ $# -eq 4 ]; then
+   NB_PAXOS_NODES=$1
+   NB_ITER=$2
+   LEADER_ACCEPTOR=$3
+   MESSAGE_MAX_SIZE=$4
+   PROFILER=
+ 
+else
+   echo "Usage: ./$(basename $0) <nb_paxos_nodes> <nb_iter> <same_proc|different_proc> <msg_max_size> [profiling?]"
+   exit 0
+fi
+
+./stop_all.sh
+rm -f /tmp/paxosInside_client_*_finished
+
+# create config file
+./create_config.sh $NB_PAXOS_NODES 2 $NB_ITER $LEADER_ACCEPTOR > $CONFIG_FILE
+
+# set new parameters
+sudo sysctl -p ../inet_sysctl.conf
+
+# compile
+echo "-DMESSAGE_MAX_SIZE=${MESSAGE_MAX_SIZE} -DTCP_NAGLE" > INET_TCP_PROPERTIES
+make inet_tcp_paxosInside
+
+
+#####################################
+############# Profiler  #############
+if [ ! -z $PROFILER ]; then
+cd $PROFDIR
+make
+cd ..
+fi
+#####################################
+
+
+# launch
+./bin/inet_tcp_paxosInside $CONFIG_FILE &
+
+
+#####################################
+############# Profiler  #############
+if [ ! -z $PROFILER ]; then
+sleep 5
+sudo $PROFDIR/profiler-sampling &
+fi
+#####################################
+
+
+# wait for the end
+nbc=0
+while [ $nbc -ne 1 ]; do
+   echo "Waiting for the end: nbc=$nbc / 1"
+   sleep 10
+
+   nbc=0
+   for i in $(seq 0 2); do
+      F=/tmp/paxosInside_client_$(($i + $NB_PAXOS_NODES))_finished
+      if [ -e $F ]; then
+         nbc=$(($nbc+1))
+      fi
+   done
+done
+
+
+#####################################
+############# Profiler  #############
+if [ ! -z $PROFILER ]; then
+sudo pkill profiler
+sudo chown bft:bft /tmp/perf.data.*
+
+OUTPUT_DIR=inet_tcp_profiling_${NB_PAXOS_NODES}nodes_2clients_${NB_ITER}iter_${MESSAGE_MAX_SIZE}B_${LEADER_ACCEPTOR}
+mkdir $OUTPUT_DIR
+
+for e in 0 1 2; do
+   $PROFDIR/parser-sampling /tmp/perf.data.* --c 0 --c 1 --c 2 --c 3 --c 4 --c 5 --c 6 --base-event ${e} --app inet_tcp_paxosI > $OUTPUT_DIR/perf_everyone_event_${e}.log
+done
+
+rm /tmp/perf.data.* -f
+fi
+#####################################
+
+
+# save results
+./stop_all.sh
+./remove_shared_segment.pl
+mv results.txt inet_tcp_${NB_PAXOS_NODES}nodes_2clients_${NB_ITER}iter_${MESSAGE_MAX_SIZE}B_${LEADER_ACCEPTOR}.txt
