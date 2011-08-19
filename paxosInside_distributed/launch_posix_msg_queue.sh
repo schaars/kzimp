f32304a074cbdf0dadaedb9f2a8f2034a647a53d
Pierre Louis Aublin <pierre-louis.aublin@inria.fr>
2011-08-19 10:45:30 +0200

    a bug with the output dir (when using bprof)

--- paxosInside_distributed/launch_posix_msg_queue.sh
+++ paxosInside_distributed/launch_posix_msg_queue.sh
@@ -110,7 +110,7 @@ OUTPUT_DIR=posix_mq_profiling_${NB_PAXOS_NODES}nodes_2clients_${NB_ITER}iter_${M
 mkdir $OUTPUT_DIR
 
 for e in 0 1 2; do
-   $PROFDIR/parser-sampling /tmp/perf.data.* -c 0 -c 1 -c 2 -c 3 -c 4 -c 5 -c 6 --base-event ${e} --app posix_msg_queu > OUTPUT_DIR/perf_everyone_event_${e}.log
+   $PROFDIR/parser-sampling /tmp/perf.data.* -c 0 -c 1 -c 2 -c 3 -c 4 -c 5 -c 6 --base-event ${e} --app posix_msg_queu > $OUTPUT_DIR/perf_everyone_event_${e}.log
 done
 
 rm /tmp/perf.data.* -f
